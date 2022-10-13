affbefa2fed10dfbcb3ba1fbffde5268263d6bff
martinalbrnz <martinalbor96@gmail.com>
2022-10-13 15:00:54 -0300

    Add isInside to date functions

--- overlapped.js
+++ overlapped.js
@@ -19,24 +19,53 @@ const overlapped = {
   end: new Date("08-16-2022 22:00:00"),
 };
 
-const nonOverlapped = {
+const notOverlapped = {
   init: new Date("08-16-2022 18:00:00"),
   end: new Date("08-16-2022 21:00:00"),
 };
 
+const inside = {
+  init: new Date("08-16-2022 16:30:00"),
+  end: new Date("08-16-2022 18:00:00"),
+}
+
+const notInside = {
+  init: new Date("08-16-2022 14:00:00"),
+  end: new Date("08-16-2022 19:00:00"),
+}  
+
+// If it overlap one or more dates
 const isOverlapped = (date, listOfDates) => {
   return listOfDates
     .map((item) => date.init < item.end === date.end > item.init)
     .some((i) => i);
 };
 
+// If it is inside another date
+const isInside = (date, listOfDates) => {
+  return listOfDates
+    .map((item) => date.init >= item.init && date.end <= item.end)
+    .some((i) => i);
+}
+
 console.time('nonOver')
-console.log("Non overlapped: ", isOverlapped(nonOverlapped, dateList)); // should be false
+console.log("Non overlapped: ", isOverlapped(notOverlapped, dateList)); // should be false
 console.timeEnd('nonOver')
+console.log('');
 
 console.time('over')
 console.log("Overlapped: ", isOverlapped(overlapped, dateList)); // should be true
 console.timeEnd('over')
+console.log('');
+
+console.time('nonInside')
+console.log("Non inside: ", isInside(notInside, dateList)); // should be false
+console.timeEnd('nonInside')
+console.log('');
+
+console.time('inside')
+console.log("Inside: ", isInside(inside, dateList)); // should be true
+console.timeEnd('inside')
 
 /**
  * Vague Javascript implementation of an algorithm to detect
